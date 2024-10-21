c149c742c3855cd7442ccef536a73de16832f1a2
deepaksomesh <deepaksomi1986@gmail.com>
2024-10-21 16:09:23 +0530

    packages

---
+++ ImageAnalyzer/IA app.bat
@@ -0,0 +1,5 @@
+set mypath=%cd%
+@echo %mypath%
+cd %mypath%\bin
+python launcher.py
+pause
---
+++ ImageAnalyzer/packages.bat
@@ -0,0 +1,6 @@
+pip install imagehash
+pip install selenium
+pip install matplotlib
+pip install imageio
+pip install opencv-python
+pip install keyboard
